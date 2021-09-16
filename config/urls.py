09b85ab976cb70ecfb15be6e45e55af2e59c38ed
Kavin Wickham <64988977+wickhamkl007@users.noreply.github.com>
2021-09-16 14:16:53 -0600

    Update

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')), # new
+    path('', include('pages.urls')), 
 ]
